17ab081ca318b7f7dff119dc83c61278a2f3933f
maximprosin <maximprosin647@gmail.com>
2026-07-11 19:44:26 +0300

    task 1.1 complete

---
+++ Tasks 1.1/main.cpp
@@ -0,0 +1,90 @@
+#include <iostream>
+
+int main() {
+    int age = 18;
+    double pi = 3.14159;
+    char letter = 'M';
+    bool is_student = true;
+
+    int a = 10;
+    int b = 20;
+    int c = a + b;
+
+    double x = 5.5;
+    double y = 2.0;
+
+    int x1 = 5;
+    int y1 = 2;
+
+    int num1 = 5;
+    int num2 = 1;
+
+    std::string name = "Maxim";
+    std::string language = "C++";
+
+    std::string first = "Hello";
+    std::string second = "World";
+
+    std::string greeting = first + " " + second + "!";
+
+    std::cout << "Hello, Arch!" << std::endl;
+    std::cout << "C++ is fun." << std::endl;
+    std::cout << "Let's code." << std::endl;
+
+    std::cout << "My age is: " << age << std::endl;
+
+    std::cout << "Pi is approximately: " << pi << std::endl;
+
+    std::cout << "First letter: " << letter << std::endl;
+
+    std::cout << "Student status: " << is_student << std::endl;
+
+    std::cout << age << " " << pi << " " << letter << " " << is_student << " " << std::endl;
+
+    std::cout << 10 + 20 << std::endl;
+    std::cout << c << std::endl;
+
+    std::cout << x / y << std::endl;
+    std::cout << x1 / y1 << std::endl;
+
+    std::cout << "My name is " << name << " and I am learning " << language << std::endl;
+
+    std::cout << greeting << std::endl;
+
+    std::cout << ".--.--.--.  .--.--." << std::endl;
+    std::cout << "|     |        |  |" << std::endl;
+    std::cout << ":  :--:  :  :  :  :" << std::endl;
+    std::cout << "|  |     |  |     |" << std::endl;
+    std::cout << ":  :  :  :--:--:--:" << std::endl;
+    std::cout << "|  |  |           |" << std::endl;
+    std::cout << ":  :  :--:--:--:  :" << std::endl;
+    std::cout << "|  |        |  |  |" << std::endl;
+    std::cout << ":  :--:--:  :  :  :" << std::endl;
+    std::cout << "|     |     |  |  |" << std::endl;
+    std::cout << ":--:  :  :--:  :  :" << std::endl;
+    std::cout << "|        |        |" << std::endl;
+    std::cout << ":  :--:--:--:--:--:" << std::endl;
+
+    std::cout << "5 x 1 = " << num1 * num2 << std::endl;
+    num2++;
+    std::cout << "5 x 2 = " << num1 * num2 << std::endl;
+    num2++;
+    std::cout << "5 x 3 = " << num1 * num2 << std::endl;
+    num2++;
+    std::cout << "5 x 4 = " << num1 * num2 << std::endl;
+    num2++;
+    std::cout << "5 x 5 = " << num1 * num2 << std::endl;
+    num2++;
+    std::cout << "5 x 6 = " << num1 * num2 << std::endl;
+    num2++;
+    std::cout << "5 x 7 = " << num1 * num2 << std::endl;
+    num2++;
+    std::cout << "5 x 8 = " << num1 * num2 << std::endl;
+    num2++;
+    std::cout << "5 x 9 = " << num1 * num2 << std::endl;
+    num2++;
+    std::cout << "5 x 10 = " << num1 * num2 << std::endl;
+    num2++;
+    
+    return 0;
+}
